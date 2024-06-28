a65bdb7da0cf0fa2098ef8f0a512d640a81e2f1b
surajpathade <spathade058@gmail.com>
2024-06-28 10:55:51 +0530

    first commit

--- src/com/javaBasics/Example1.java
+++ src/com/javaBasics/Example1.java
@@ -1,11 +1,27 @@
 package com.javaBasics;
+import java.util.Scanner;
 
 public class Example1 {
 
 	public static void main(String[] args) {
+		 
+		 Scanner sc= new Scanner(System.in);
+		 System.out.println("enter your first num");
+		int a = sc.nextInt();
+		System.out.println("enter your second num");
+		int b = sc.nextInt();
 		
-		System.out.println("First Program");
+		int sum = a+b;
 
+		System.out.println("the sum is " +sum);
+		
+		
+		
+				 
+		 
+		 		 
+	
+		
 	}
 
 }
